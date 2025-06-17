c8a090880dc07ed078a18ccf408d5c2ac0ef2358
Camilo Galindo <157303041+jucamilogal@users.noreply.github.com>
2025-06-17 08:00:59 -0500

    Cursos Platzi

--- API/app.py
+++ API/app.py
@@ -14,3 +14,8 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+@app.get("/cursosPlatzi")
+def get_cursos():
+    rows =  ["Curso de Python", "Curso de JavaScript", "Curso de React", "Curso de Node.js", "Curso de Data Science"]
+    return rows
